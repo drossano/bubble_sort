35af06f5e62f7a1b6a581f3f619b84b72191b7f1
drossano <72749793+drossano@users.noreply.github.com>
2021-11-04 16:16:28 -0400

    Create script.rb and write pseudocode in comments

---
+++ srcipt.rb
@@ -0,0 +1,7 @@
+def bubble_sort(array)
+  #Takes aray and sorts from low to high
+  #Compares 2 elements, if the one on the left is greater than the one on the right they swap.
+  #Repeats this process until the array is sorted.
+end
+
+bubble_sort([4,3,78,2,0,2])
