5f35dd7668ccf6f7389771d260e7b089fd16f934
drossano <72749793+drossano@users.noreply.github.com>
2021-11-04 22:51:29 -0400

    Add functionality to sort until no items need to be sorted anymore

--- script.rb
+++ script.rb
@@ -1,15 +1,17 @@
 def bubble_sort(array)
-  #Takes aray and sorts from low to high
-  #Compares 2 elements, if the one on the left is greater than the one on the right they swap.
-  #Repeats this process until the array is sorted.
-  #Loops through array 2 items at a time
-  index = 0
-  
-  while index < array.length()
-    if (array[index] <=> array [index + 1]) == 1 
-      array = array.insert(index, array.delete_at(index + 1))
+  loop do
+    index = 0
+    swaps = 0
+    while index < array.length()
+      if (array[index] <=> array [index + 1]) == 1 
+        array = array.insert(index, array.delete_at(index + 1))
+        swaps += 1
+      end
+      index += 1
     end
-    index += 1
+    if swaps == 0
+      break
+    end
   end
   print array
 end
